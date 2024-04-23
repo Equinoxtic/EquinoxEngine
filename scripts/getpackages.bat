748dfbed1a910d473becb4547fa3f4864421acab
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-04-23 22:09:57 +0800

    Create building scripts

---
+++ scripts/getpackages.bat
@@ -0,0 +1,25 @@
+@echo off
+cd ..
+@echo on
+echo GETTING PACKAGES FOR BUILDING
+haxelib install flixel 5.1.0
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.0
+haxelib install flixel-ui 2.6.1
+haxelib install flixel-addons 3.0.0
+haxelib install flixel-tools 1.5.1
+haxelib install flixel-demos 3.1.0
+haxelib install flixel-templates 2.7.0
+haxelib install openfl-samples 8.7.0
+haxelib install lime-samples 7.0.0
+haxelib install actuate 1.9.0
+haxelib install hxcpp 4.3.2
+haxelib install layout 1.2.1
+haxelib install box2d 1.2.3
+haxelib install hscript 2.5.0
+haxelib install systools 1.1.0
+haxelib install random 1.4.1
+haxelib install hxCodec 2.5.1
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git fnf-modcharting-tools https://github.com/TheZoroForce240/FNF-Modcharting-Tools

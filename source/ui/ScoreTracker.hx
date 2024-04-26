38c13356366d7054e681473dc45ec0debaee2951
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-04-26 21:50:09 +0800

    [Minor]: Update ScoreTracker text
    * Update ScoreTracker font, changed vertical bars (|) to slashes (/)
    * Added text for when player goes into BOTPLAY or PRACTICE MODE

--- source/ui/ScoreTracker.hx
+++ source/ui/ScoreTracker.hx
@@ -13,7 +13,9 @@ class ScoreTracker extends FlxSpriteGroup
 
 	private static var scoreTxt:FlxText;
 
-	public function new(?x:Float = 0, ?y:Float = 0, ?instance:FlxBasic, ?fontSize = 24, ?alignment:FlxTextAlign = CENTER) {
+	public var fontSize:Int = 20;
+
+	public function new(?x:Float = 0, ?y:Float = 0, ?instance:FlxBasic, ?fontSize:Int = 20, ?alignment:FlxTextAlign = CENTER) {
 		super();
 
 		if (instance == null) {
@@ -21,18 +23,37 @@ class ScoreTracker extends FlxSpriteGroup
 		}
 
 		this.instance = instance;
+		this.fontSize = fontSize;
 
-		scoreTxt = new FlxText(x, /**healthBarBG.y + 42*/ y, FlxG.width, "", fontSize);
-		scoreTxt.setFormat(Paths.font('squareone.ttf'), fontSize, FlxColor.WHITE, alignment, FlxTextBorderStyle.OUTLINE, 0xFF000000);
-		scoreTxt.borderSize = 1.275;
+		scoreTxt = new FlxText(x, y, FlxG.width, "", fontSize);
+		scoreTxt.setFormat(Paths.font('azonix.otf'), fontSize, FlxColor.WHITE, alignment, FlxTextBorderStyle.OUTLINE, 0xFF000000);
+		scoreTxt.borderSize = 1.3;
 		scoreTxt.antialiasing = ClientPrefs.globalAntialiasing;
 		add(scoreTxt);
 	}
 
-	public function updateScoreText(?score:Int = 0, ?misses:Int = 0, ?rating: String = "", ?ratingFC: String = "", ?ratingPercent:Float = 0.0) {
-		scoreTxt.text = 'Score: ' + score
-		+ ' | Misses: ' + misses
-		+ ' | Rating: ' + rating
-		+ (rating != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
+	public function updateScoreText(?score:Int = 0, ?misses:Int = 0, ?ratingFC:String = "", ?rankingString:String = "", ?ratingPercent:Float = 0.0) {
+
+		// < SCORE: SONG SCORE / COMBO BREAKS: MISSES / ACCURACY: ACCURACY% / RATING - RANK >
+		scoreTxt.text = '< SCORE: ${score}'
+		+ ' / COMBO BREAKS: ${misses}'
+		+ ' / '
+		+ ((score > 0) ? 'ACCURACY: ${Highscore.floorDecimal(ratingPercent * 100, 2)}% / $ratingFC - ${rankingString}' : '')
+		+ ' >';
+		/**
+		 * NOTE: I used the condition (SCORE > 0) to make it seem more feature-proof than using (RATING or RANKING != X)
+		 */
+
+		if (PlayState.instance.cpuControlled) {
+			scoreTxt.size = Std.int(fontSize * 1.1);
+			scoreTxt.text = '< BOTPLAY >';
+		} else if (PlayState.instance.practiceMode) {
+			scoreTxt.size = Std.int(fontSize * 1.1);
+			scoreTxt.text = '< PRACTICE MODE >';
+		}
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
 	}
 }

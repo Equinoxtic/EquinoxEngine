f208333a8384e9f891c437f7b8475b65c7fd0940
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-04-28 01:35:13 +0800

    [Minor]: Update JudgementCounter text handling and content

--- source/ui/JudgementCounter.hx
+++ source/ui/JudgementCounter.hx
@@ -12,6 +12,17 @@ using StringTools;
 class JudgementCounter extends FlxSpriteGroup
 {
 	public var colorArray:Array<Int> = [225, 235, 95];
+
+	private var judgementTextList:Array<String> = [
+		'TOTAL HITS',
+		'COMBO',
+		'SICK',
+		'GOOD',
+		'BAD',
+		'SHIT',
+		'COMBO BREAKS'
+	];
+
 	var instance:FlxBasic;
 
 	private static var judgementTxt:FlxText;
@@ -24,7 +35,7 @@ class JudgementCounter extends FlxSpriteGroup
 		}
 
 		this.instance = instance;
-		
+
 		judgementTxt = new FlxText(x, y, FlxG.width, "", fontSize);
 		judgementTxt.setFormat(Paths.font('azonix.otf'), fontSize, FlxColor.WHITE, alignment, FlxTextBorderStyle.OUTLINE, 0xFF000000);
 		judgementTxt.borderSize = 1.45;
@@ -33,11 +44,14 @@ class JudgementCounter extends FlxSpriteGroup
 		add(judgementTxt);
 	}
 
-	public function updateJudgementCounter(?sicksValue:Int = 0, ?goodsValue:Int = 0, ?badsValue:Int = 0, ?shitsValue:Int = 0) {
-		judgementTxt.text = 'Sick: ${sicksValue}\n\n' +
-							'Good: ${goodsValue}\n\n' +
-							'Bad: ${badsValue}\n\n' +
-							'Shit: ${shitsValue}';
+	public function updateJudgementCounter(?hitsValue:Int = 0, ?combosValue = 0, ?sicksValue:Int = 0, ?goodsValue:Int = 0, ?badsValue:Int = 0, ?shitsValue:Int = 0, ?missesValue:Int = 0) {
+		judgementTxt.text = '${judgementTextList[0]}: ${hitsValue}\n' +
+							'${judgementTextList[1]}: ${combosValue}\n\n' +
+							'${judgementTextList[2]} ${sicksValue}\n' +
+							'${judgementTextList[3]}: ${goodsValue}\n' +
+							'${judgementTextList[4]}: ${badsValue}\n' +
+							'${judgementTextList[5]}: ${shitsValue}\n\n' +
+							'${judgementTextList[6]}: ${missesValue}';
 	}
 
 	override function update(elapsed:Float) {

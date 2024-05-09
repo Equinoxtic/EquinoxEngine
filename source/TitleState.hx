b00769b8e17ffb50e0b9790667dd1ee3a02021c0
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-05-09 19:53:31 +0800

    [Patch]: Update HTTPS version link

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -138,7 +138,7 @@ class TitleState extends MusicBeatState
 		#if CHECK_FOR_UPDATES
 		if(ClientPrefs.checkForUpdates && !closedState) {
 			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/ShadowMario/FNF-PsychEngine/main/gitVersion.txt");
+			var http = new haxe.Http("https://raw.githubusercontent.com/Equinoxtic/SolariumEngine/master/SolariumVersion.txt");
 
 			http.onData = function (data:String)
 			{

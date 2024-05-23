68c9d71dae0a2b3e46589c2366620f3734079ed0
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-05-23 15:26:00 +0800

    [Minor]: Separate Rating class from Conductor.hx.

--- source/Conductor.hx
+++ source/Conductor.hx
@@ -1,6 +1,7 @@
 package;
 
 import Song.SwagSong;
+import util.Rating;
 
 /**
  * ...
@@ -152,33 +153,5 @@ class Conductor
 
 		crochet = calculateCrochet(bpm);
 		stepCrochet = crochet / 4;
-	}
-}
-
-class Rating
-{
-	public var name:String = '';
-	public var image:String = '';
-	public var counter:String = '';
-	public var hitWindow:Null<Int> = 0; //ms
-	public var ratingMod:Float = 1;
-	public var score:Int = 500;
-	public var noteSplash:Bool = true;
-
-	public function new(name:String)
-	{
-		this.name = name;
-		this.image = name;
-		this.counter = name + 's';
-		this.hitWindow = Reflect.field(ClientPrefs, name + 'Window');
-		if(hitWindow == null)
-		{
-			hitWindow = 0;
-		}
-	}
-
-	public function increase(blah:Int = 1)
-	{
-		Reflect.setField(PlayState.instance, counter, Reflect.field(PlayState.instance, counter) + blah);
 	}
 }
---
+++ source/util/Rating.hx
@@ -0,0 +1,32 @@
+package util;
+
+import util.Constants as Constants;
+
+class Rating
+{
+	public var name:String = '';
+	public var image:String = '';
+	public var counter:String = '';
+	public var hitWindow:Null<Int> = 0; //ms
+	public var ratingMod:Float = 1;
+	public var score:Int = 700;
+	public var healthGain:Float = Constants.HEALTH_MARVELOUS_BONUS;
+	public var noteSplash:Bool = true;
+
+	public function new(name:String)
+	{
+		this.name = name;
+		this.image = name;
+		this.counter = name + 's';
+		this.hitWindow = Reflect.field(ClientPrefs, name + 'Window');
+		if(hitWindow == null)
+		{
+			hitWindow = 0;
+		}
+	}
+
+	public function increase(blah:Int = 1)
+	{
+		Reflect.setField(PlayState.instance, counter, Reflect.field(PlayState.instance, counter) + blah);
+	}
+}

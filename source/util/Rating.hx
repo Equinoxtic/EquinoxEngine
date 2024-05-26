0ff9be549932f88504b05b21aadd8e19d1cdb308
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-05-26 17:22:16 +0800

    [Minor]: Add PlayStateRating class to Rating.hx.

--- source/util/Rating.hx
+++ source/util/Rating.hx
@@ -30,3 +30,58 @@ class Rating
 		Reflect.setField(PlayState.instance, counter, Reflect.field(PlayState.instance, counter) + blah);
 	}
 }
+
+class PlayStateRating
+{
+	public static function initPlayStateRatings(dataArray:Null<Array<Rating>>):Void
+	{
+		if (dataArray != null || !(dataArray.length < 0))
+		{
+			/**
+			 * 'Marvelous!' Rating.
+			 */
+			dataArray.push(new Rating('marv'));
+
+			/**
+			 * 'Sick!' Rating.
+			 */
+			var ratingSick:Rating = new Rating('sick');
+			ratingSick.ratingMod = 1.0;
+			ratingSick.score = 350;
+			ratingSick.healthGain = Constants.HEALTH_SICK_BONUS;
+			ratingSick.noteSplash = true;
+		
+			/**
+			 * 'Good!' Rating.
+			 */
+			var ratingGood:Rating = new Rating('good');
+			ratingGood.ratingMod = 0.7;
+			ratingGood.score = 200;
+			ratingGood.healthGain = Constants.HEALTH_GOOD_BONUS;
+			ratingGood.noteSplash = false;
+			
+			/**
+			 * 'Bad.' Rating.
+			 */
+			var ratingBad:Rating = new Rating('bad');
+			ratingBad.ratingMod = 0.4;
+			ratingBad.score = 100;
+			ratingBad.healthGain = Constants.HEALTH_BAD_BONUS;
+			ratingBad.noteSplash = false;
+			
+			/**
+			 * 'Shit.' Rating.
+			 */
+			var ratingShit:Rating = new Rating('shit');
+			ratingShit.ratingMod = 0;
+			ratingShit.score = 50;
+			ratingShit.healthGain = Constants.HEALTH_SHIT_BONUS;
+			ratingShit.noteSplash = false;
+			
+			dataArray.push(ratingSick);
+			dataArray.push(ratingGood);
+			dataArray.push(ratingBad);
+			dataArray.push(ratingShit);
+		}
+	}
+}

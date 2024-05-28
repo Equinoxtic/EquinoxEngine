efd516cc56f4b9cfffdb146bfaf5319a0c60b324
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-05-28 18:58:32 +0800

    [Minor]: Rewrite and update Checkerboard class

--- source/misc/Checkerboard.hx
+++ source/misc/Checkerboard.hx
@@ -19,37 +19,69 @@ enum CheckerboardSizeType
 class Checkerboard extends FlxBackdrop
 {
 	/**
-	 * Create a new Checker Background, using FlxBackdrop.
+	 * The default path for the 'checkers' asset.
+	 */
+	private var defaultCheckersPath:String = 'ui/menu/checkers/checker';
+
+	/**
+	 * The size of each tile checkerboard. Larger sizes may cause performance and loading issues!
+	 */
+	public var tileSize:CheckerboardSizeType = NORMAL;
+
+	/**
+	 * Create a new Checker Background, uses and extends [``FlxBackdrop``](https://api.haxeflixel.com/flixel/addons/display/FlxBackdrop.html).
+	 * 
 	 * @param axes The axes on which to repeat. The default, XY will tile the entire camera.
 	 * @param spacing Amount of spacing between tiles on the X and Y axis.
-	 * @param sizeType The size of each tile checkerboard. Larger sizes may cause performance and loading issues!
+	 * @param tileSize The size of each tile checkerboard. Larger sizes may cause performance and loading issues!
 	 * @param checkerAlpha The alpha of each tile of the checkerboard.
 	 * @param checkerColor The color of each tile of the checkerboard.
 	 * @param scrollX The scrollfactor on the X axis.
 	 * @param scrollY the scrollfactor on the Y axis.
 	 */
-	public function new(?axes:FlxAxes = XY, ?spacing:Int = 0, ?sizeType:CheckerboardSizeType = NORMAL, ?checkerAlpha:Float = 0.5, ?checkerColor:FlxColor = 0xFF000000, ?scrollX:Float = 0.0, ?scrollY:Float = 0.07):Void
+	public function new(?axes:FlxAxes = XY, ?spacing:Int = 0, ?tileSize:CheckerboardSizeType = NORMAL, ?checkerAlpha:Float = 0.5, ?checkerColor:FlxColor = 0xFF000000, ?scrollX:Float = 0.0, ?scrollY:Float = 0.07):Void
 	{
-		super(Paths.image(Std.string('ui/menu/checkers/checker${getSizePrefixBySizeType(sizeType)}')), axes, spacing, spacing);
+		super(Paths.image(Std.string('${defaultCheckersPath}${getTileSize(tileSize)}')), axes, spacing, spacing);
+
 		color = checkerColor;
 		alpha = checkerAlpha;
+		
+		if (tileSize == null)
+			tileSize = NORMAL;
+		this.tileSize = tileSize;
+		
 		scrollFactor.set(scrollX, scrollY);
 	}
 
-	private function getSizePrefixBySizeType(sizeType:CheckerboardSizeType = NORMAL):String
+	/**
+	 * Gets the size prefix from a Checkerboard's size type.
+	 * @param tileSize 
+	 * @return String
+	 */
+	private function getTileSize(tileSizeType:CheckerboardSizeType = NORMAL):String
 	{
-		var sizePrefix:String = '32';
-		switch (sizeType)
+		var r:String = '32';
+
+		switch (tileSizeType)
 		{
-			case SMALL: sizePrefix = '16';
-			case NORMAL: sizePrefix = '32';
-			case LARGE: sizePrefix = '64';
-			case HUGE: sizePrefix = '128';
-			case EXTRA_HUGE: sizePrefix = '256';
+			case SMALL: r = '16';
+			case NORMAL: r = '32';
+			case LARGE: r = '64';
+			case HUGE: r = '128';
+			case EXTRA_HUGE: r = '256';
 		}
-		return '_${Std.string(sizePrefix)}';
+
+		return '_${Std.string(r)}';
 	}
 
+	/**
+	 * Updates and increments the position of the Checkerboard.
+	 * 
+	 * (Use this under the ``update()`` function of a state.)
+	 * 
+	 * @param ix The amount of increments in the x axis.
+	 * @param iy The amount of increments in the y axis.
+	 */
 	public function updatePosition(ix:Float = 0.47, iy:Float = 0.16):Void
 	{
 		x -= ix / (ClientPrefs.framerate / 60);

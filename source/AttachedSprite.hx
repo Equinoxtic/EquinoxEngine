c45cb6d1338a98e8318e11fe3e538b8b668b4cf2
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-04 21:01:21 +0800

    [Minor]: Just gonna fix the indentation on AttachedSprite.hx

--- source/AttachedSprite.hx
+++ source/AttachedSprite.hx
@@ -16,35 +16,37 @@ class AttachedSprite extends FlxSprite
 	public var copyAlpha:Bool = true;
 	public var copyVisible:Bool = false;
 
-	public function new(?file:String = null, ?anim:String = null, ?library:String = null, ?loop:Bool = false)
+	public function new(?file:String = null, ?anim:String = null, ?library:String = null, ?loop:Bool = false):Void
 	{
 		super();
-		if(anim != null) {
+		if (anim != null)
+		{
 			frames = Paths.getSparrowAtlas(file, library);
 			animation.addByPrefix('idle', anim, 24, loop);
 			animation.play('idle');
-		} else if(file != null) {
+		}
+		else if (file != null)
+		{
 			loadGraphic(Paths.image(file));
 		}
+
 		antialiasing = ClientPrefs.globalAntialiasing;
+
 		scrollFactor.set();
 	}
 
-	override function update(elapsed:Float)
+	override function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-
-		if (sprTracker != null) {
+		if (sprTracker != null)
+		{
 			setPosition(sprTracker.x + xAdd, sprTracker.y + yAdd);
 			scrollFactor.set(sprTracker.scrollFactor.x, sprTracker.scrollFactor.y);
-
-			if(copyAngle)
+			if (copyAngle)
 				angle = sprTracker.angle + angleAdd;
-
-			if(copyAlpha)
+			if (copyAlpha)
 				alpha = sprTracker.alpha * alphaMult;
-
-			if(copyVisible) 
+			if (copyVisible) 
 				visible = sprTracker.visible;
 		}
 	}

047ae3bf88d1a1bb7bf397e13b85dbe546a485dc
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-05 17:12:02 +0800

    [Patch]: Update setup scripts.

--- scripts/setup-unix.sh
+++ scripts/setup-unix.sh
@@ -1,8 +1,10 @@
-@echo off
-cd ..
-@echo on
-echo GETTING PACKAGES FOR BUILDING
-haxelib install flixel 5.2.1
+#!/bin/bash
+
+echo "Getting packages for building..."
+
+haxelib setup ~/haxelib
+
+haxelib --never install flixel 5.2.1
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.0
 haxelib install flixel-ui 2.6.1
@@ -13,7 +15,7 @@ haxelib install flixel-templates 2.7.0
 haxelib install openfl-samples 8.7.0
 haxelib install lime-samples 7.0.0
 haxelib install actuate 1.9.0
-haxelib install hxcpp 4.3.2
+haxelib install hxcpp 4.2.1
 haxelib install layout 1.2.1
 haxelib install box2d 1.2.3
 haxelib install hscript 2.5.0
--- scripts/setup-windows.bat
+++ scripts/setup-windows.bat
@@ -1,8 +1,9 @@
-#!/bin/bash
+@echo off
+echo Getting packages for building...
 
-echo "Getting packages for building..."
+haxelib setup C:/haxelib
 
-haxelib install flixel 5.2.1
+haxelib --never install flixel 5.2.1
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.0
 haxelib install flixel-ui 2.6.1
@@ -13,7 +14,7 @@ haxelib install flixel-templates 2.7.0
 haxelib install openfl-samples 8.7.0
 haxelib install lime-samples 7.0.0
 haxelib install actuate 1.9.0
-haxelib install hxcpp 4.3.2
+haxelib install hxcpp 4.2.1
 haxelib install layout 1.2.1
 haxelib install box2d 1.2.3
 haxelib install hscript 2.5.0
@@ -21,3 +22,5 @@ haxelib install systools 1.1.0
 haxelib install hxCodec 2.5.1
 haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+echo Finished installing packages.
+pause

9bd3b3cc8556f23f8f225180d0dedb49f4b03d96
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-08 16:43:23 +0800

    Add ColorTween for GlobalTweenClass.hx

--- source/funkin/tweens/GlobalTweenClass.hx
+++ source/funkin/tweens/GlobalTweenClass.hx
@@ -1,5 +1,8 @@
 package funkin.tweens;
 
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+import flixel.tweens.misc.ColorTween;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
 import flixel.tweens.misc.VarTween;
@@ -13,5 +16,10 @@ class GlobalTweenClass
 	public static function tween(Object:Dynamic, Values:Dynamic, Duration:Float = 1, ?Options:TweenOptions):VarTween
 	{
 		return globalManager.tween(Object, Values, Duration, Options);
+	}
+
+	public static function color(Sprite:FlxSprite, Duration:Float = 1.0, FromColor:FlxColor = 0xFFFFFFFF, ToColor:FlxColor = 0xFFFFFFFF, ?Options:TweenOptions):ColorTween
+	{
+		return globalManager.color(Sprite, Duration, FromColor, ToColor, Options);
 	}
 }

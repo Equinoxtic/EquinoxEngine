461684663aa34662acdde3efd54a23385b3234c9
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-08 16:43:03 +0800

    Finish code for StatisticsText.hx and delete old text classes

--- source/funkin/play/hud/game/StatisticsHUD.hx
+++ source/funkin/play/hud/game/StatisticsHUD.hx
@@ -1,20 +1,18 @@
 package funkin.play.hud.game;
 
+import flixel.FlxSprite;
 import flixel.FlxBasic;
-import flixel.text.FlxText;
-import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import flixel.group.FlxSpriteGroup;
-import funkin.play.scoring.*;
-import funkin.play.hud.game.statistics.*;
+import funkin.play.hud.game.statistics.StatisticsText;
 
-class StatisticsHUD extends FlxSpriteGroup
+class StatisticsHUD extends FlxTypedSpriteGroup<FlxSprite>
 {
 	private var instance:FlxBasic;
 
-	private var scoreText:ScoreText;
-	private var missesText:ComboBreaksText;
-	private var accuracyText:AccuracyText;
-	private var ratingText:RatingText;
+	private var scoreText:StatisticsText;
+	private var missesText:StatisticsText;
+	private var accuracyText:StatisticsText;
+	private var ratingText:StatisticsText;
 
 	/**
 	 * Creates the HUD for the player's score/statistics. (Shows the Score, Misses, Accuracy, and Rating)
@@ -38,74 +36,19 @@ class StatisticsHUD extends FlxSpriteGroup
 		FlxG.log.add('Created Statistics HUD.');
 		#end
 
-		scoreText = new ScoreText(X, Y);
-		scoreText.scoreString = 'Score';
-		scoreText.scoreNum = PlayState.instance.songScore;
-
-		missesText = new ComboBreaksText(scoreText.x, scoreText.y + 25);
-		missesText.comboBreaksString = 'Misses';
-		missesText.comboBreaksNum = PlayState.instance.songMisses;
-
-		accuracyText = new AccuracyText(missesText.x, missesText.y + 25);
-		accuracyText.accuracyString = 'Accuracy';
-		accuracyText.accuracyNum = Highscore.floorDecimal(PlayState.instance.ratingPercent * 100, 2);
-		accuracyText.shouldRound = false;
-
-		ratingText = new RatingText(accuracyText.x, accuracyText.y + 25);
-		ratingText.ratingString = PlayState.instance.ratingFC;
-		ratingText.rankingString = PlayState.instance.ranking;
-
-		add(ratingText);
-		add(accuracyText);
-		add(missesText);
+		scoreText = new StatisticsText(X, Y, SCORE);
+		missesText = new StatisticsText(scoreText.x, scoreText.y + 25, MISSES);
+		accuracyText = new StatisticsText(missesText.x, missesText.y + 25, ACCURACY);
+		ratingText = new StatisticsText(accuracyText.x, accuracyText.y + 25, RATING);
+		
 		add(scoreText);
+		add(missesText);
+		add(accuracyText);
+		add(ratingText);
 	}
 
-	/**
-	 * Sets all numerical values of each statistic. (i.e. The player's score, etc.)
-	 * @param songScore The player's score during a song.
-	 * @param songMisses The amount of misses that the player has.
-	 * @param accuracy The accuracy of the player during a song.
-	 * @param rating The rating of the player during a song.
-	 * @param ranking The ranking of the player during a song.
-	 */
-	public function setNumValues(songScore:Int, songMisses:Int, accuracy:Float, rating:String, ranking:String):Void
+	override function update(elapsed:Float):Void
 	{
-		scoreText.scoreNum = songScore;
-		missesText.comboBreaksNum = songMisses;
-		accuracyText.accuracyNum = accuracy;
-		ratingText.ratingString = rating;
-		ratingText.rankingString = ranking;
-	}
-
-	/**
-	 * Updates all numerical values for each statistic.
-	 */
-	public function updateNumValues():Void
-	{
-		scoreText.updateScore();
-		missesText.updateComboBreaks();
-		accuracyText.updateAccuracy();
-		ratingText.updateRating();
-	}
-
-	/**
-	 * First sets the new numerical values and updates the displayed statistics.
-	 * @param autoSet Whether each update call should be called to set automatically.
-	 */
-	public function updateStatistics(?autoSet:Bool = true):Void
-	{
-		if (autoSet)
-		{
-			setNumValues(
-				PlayState.instance.songScore,
-				PlayState.instance.songMisses,
-				Highscore.floorDecimal(PlayState.instance.ratingPercent * 100, 2),
-				PlayState.instance.ratingFC,
-				PlayState.instance.ranking
-			);
-		}
-
-		updateNumValues();
+		super.update(elapsed);
 	}
 }
--- source/funkin/play/hud/game/statistics/StatisticsText.hx
+++ source/funkin/play/hud/game/statistics/StatisticsText.hx
@@ -1,32 +1,107 @@
 package funkin.play.hud.game.statistics;
 
+import funkin.play.scoring.Highscore;
+import funkin.tweens.GlobalTweenClass;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+
 using StringTools;
+
+enum StatisticType
+{
+	SCORE;
+	MISSES;
+	ACCURACY;
+	RATING;
+}
 
 class StatisticsText extends FunkinText
 {
-	public var keyStatistic:String = "score";
+	public var statisticType:StatisticType = SCORE;
 
-	public function new(?X:Float = 0.0, ?Y:Float, keyStatistic:Null<String>):Void
+	/**
+	 * Create a new text for player-specified statistics.
+	 * @param X The X position of the text.
+	 * @param Y The Y position of the text
+	 * @param statisticType The type of statistic to display. [Default: SCORE]
+	 */
+	public function new(?X:Float = 0.0, ?Y:Float, ?statisticType:Null<StatisticType> = SCORE):Void
 	{
 		super(X, Y, FlxG.width, "", Constants.STATISTICS_FONT_SIZE, LEFT, true);
 
 		this.borderSize = Constants.STATISTICS_BORDER_SIZE;
+		this.scrollFactor.set();
 
-		if (keyStatistic == null) keyStatistic = "score";
+		if (statisticType == null)
+			statisticType = StatisticType.SCORE;
 
-		this.keyStatistic = keyStatistic;
+		this.statisticType = statisticType;
 
-		switch (keyStatistic.toLowerCase().trim())
+		displayStatistic(statisticType);
+	}
+
+	private function displayStatistic(statisticType:Null<StatisticType>):Void
+	{
+		if (statisticType == null) return;
+
+		switch (statisticType)
 		{
-			case "score": // TODO: Add keyStatistic for displaying score.
-			case "misses": // TODO: Add keyStatistic for displaying misses/combo breaks.
-			case "accuracy": // TODO: Add keyStatistic for displaying accuracy.
-			case "rating": // TODO: Add keyStatistic for displaying the rating.
+			case SCORE:
+				var value:Int = PlayState.instance.songScore;
+				if (!Math.isNaN(value)) {
+					this.text = 'Score: ${value}';
+				}
+
+			case MISSES:
+				var value:Int = PlayState.instance.songMisses;
+				if (!Math.isNaN(value) && value >= 0)
+				{
+					if (value >= Constants.GLOBAL_NUMBER_CAP)
+						value = Constants.GLOBAL_NUMBER_CAP;
+					this.text = 'Misses: ${value}';
+				}
+
+			case ACCURACY:
+				var value:Float = Highscore.floorDecimal(PlayState.instance.ratingPercent * 100, 2);
+				if (!Math.isNaN(value)) {
+					this.text = 'Accuracy: ${value}%';
+				}
+			
+			case RATING:
+				var rating:String = PlayState.instance.ratingFC;
+				var ranking:String = PlayState.instance.ranking;
+				if (rating != null && ranking != null) {
+					this.text = '${rating} - ${ranking}';
+				}
 		}
 	}
+
+	private var missTween:FlxTween;
 
 	override function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+		if (!PlayState.instance.cpuControlled && !PlayState.instance.practiceMode && !PlayState.chartingMode)
+		{
+			displayStatistic(statisticType);
+		}
+		else
+		{
+			switch (statisticType)
+			{
+				case SCORE:
+					this.text =
+						"- " + ((PlayState.instance.cpuControlled) ? " BOTPLAY -" : "")
+						+ ((PlayState.instance.practiceMode) ? " PRACTICE MODE -" : "")
+						+ ((PlayState.chartingMode) ? " CHARTING MODE -" : "");
+				case MISSES:
+					this.text = "Misses: N/A";
+				case ACCURACY:
+					this.text = "Accuracy: N/A";
+				case RATING:
+					this.text = "CLEAR - N/A";
+			}
+		}
 	}
 }

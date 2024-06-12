7b4789c2e5eb618cceb599dbc620e3948759644f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-12 18:35:26 +0800

    Fix Grayscale Shader

--- source/funkin/graphics/shaders/Grayscale.hx
+++ source/funkin/graphics/shaders/Grayscale.hx
@@ -31,7 +31,7 @@ class GrayscaleShader extends FlxShader
 		{
 			vec2 uv = openfl_TextureCoordv;
 			vec4 color = flixel_texture2D(bitmap, uv);
-			float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114))
+			float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
 			gl_FragColor = mix(color, vec4(gray, gray, gray, color.a), strength);
 		}
 	')

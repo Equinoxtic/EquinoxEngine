71a928052470f857dcb2d937b6001578de3036d6
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-17 21:31:00 +0800

    Update Http.hx

--- source/funkin/util/Http.hx
+++ source/funkin/util/Http.hx
@@ -13,30 +13,31 @@ class Http
 {
 	public static var userAgent:String = 'request';
 
-	public static function requestStringFrom(url:String)
+	public static function requestStringFrom(url:String):String
 	{
 		var value:String = null;
 
-		if (url != null && url != '')
-		{
-			var httpData:HaxeHTTP = new HaxeHTTP(url);
-
-			httpData.setHeader('User-Agent', userAgent);
-
-			httpData.onData = function(data:String):Void
-			{
-				if (value == null) {
-					value = data.split('\n')[0].trim();
-				}
-			}
-
-			httpData.onError = function(err:Dynamic):Void
-			{
-				throw err;
-			}
-
-			httpData.request();
+		if (url == null && url == '') {
+			return "";
 		}
+
+		var httpData:HaxeHTTP = new HaxeHTTP(url);
+
+		httpData.setHeader('User-Agent', userAgent);
+
+		httpData.onData = function(data:String):Void
+		{
+			if (value == null) {
+				value = data.split('\n')[0].trim();
+			}
+		}
+
+		httpData.onError = function(err:Dynamic):Void
+		{
+			throw err;
+		}
+
+		httpData.request();
 
 		return value;
 	}

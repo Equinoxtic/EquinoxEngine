b28bcdef136a3eb76a5cd9d1b3b52000b5cf0969
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-18 00:49:45 +0800

    Fix FunkinText not being able to update font faces

--- source/funkin/ui/display/FunkinText.hx
+++ source/funkin/ui/display/FunkinText.hx
@@ -4,15 +4,7 @@ import flixel.text.FlxText;
 
 class FunkinText extends FlxText
 {
-	/**
-	 * Uses / forces the font that you want to load in instead of the engine's built-in fonts.
-	 */
-	public var usePreferredFont:Bool = false;
-
-	/**
-	 * The string of the preferred font to use.
-	 */
-	public var preferredFont:String = 'phantommuff.ttf';
+	public var forceDefaultFont:Bool = false;
 
 	/**
 	 * Create a new text field that extends with [``FlxText``](https://api.haxeflixel.com/flixel/text/FlxText.html).
@@ -24,47 +16,49 @@ class FunkinText extends FlxText
 	 * @param textAlignment The alignment of the text field.
 	 * @param border Should the text field have a border?
 	 */
-	public function new(X:Float, Y:Float, fieldWidth:Float = 0, ?text:String = "", ?fontSize:Int = 16, ?textAlignment:FlxTextAlign, ?border:Bool = true, ?borderSize:Float = 2.5):Void
+	public function new(X:Float, Y:Float, fieldWidth:Float = 0, ?text:String = "", ?fontSize:Int = 16, ?textAlignment:FlxTextAlign = CENTER, ?border:Bool = true, ?borderSize:Float = 2.5):Void
 	{
 		super(X, Y, fieldWidth, text, fontSize);
 
 		var fontString:String = 'phantommuff';
 		var fontExt:String = 'ttf';
 
-		switch(Preferences.fontFace) {
-			case 'Classic':
-				fontString = 'vcr';
-			case 'Engine Legacy':
-				fontString = 'azonix';
-				fontExt = 'otf';
-			default:
-				fontString = 'phantommuff';
-		}
-
-		if (PlayState.isPixelStage) {
-			fontString = 'vcr';
-		} else {
-			if (this.usePreferredFont) {
-				fontString = this.preferredFont;
+		if (!forceDefaultFont)
+		{
+			switch(Preferences.fontFace)
+			{
+				case 'Default': // Default Equinox Engine font.
+					fontString = 'phantommuff';
+				case 'Classic': // Classic FNF Font.
+					fontString = 'vcr';
+				case 'Engine Legacy': // Legacy Equinox Engine font.
+					fontString = 'azonix';
+					fontExt = 'otf';
+				default:
+					fontString = 'phantommuff';
 			}
+
+			if (PlayState.isPixelStage)
+				fontString = 'vcr';
 		}
 
-		if (!this.usePreferredFont) {
-			setFormat(Paths.font('${fontString}.${fontExt}'), fontSize, 0xFFFFFFFF, textAlignment);
-		} else {
-			setFormat(Paths.font(fontString), fontSize, 0xFFFFFFFF, textAlignment);
-		}
+		setFormat(Paths.font(Std.string('$fontString.$fontExt')), fontSize, 0xFFFFFFFF, textAlignment);
 
-		if (border) {
-			this.setBorderStyle(OUTLINE, 0xFF000000, 0.0, 5);
-
+		if (border)
+		{
+			setBorderStyle(OUTLINE, 0xFF000000, 0.0, 5);
 			if (!(borderSize < 0)) {
 				this.borderSize = borderSize;
 			} else {
-				this.borderSize = 2.5;
+				borderSize = 2.5;
 			}
 		}
 
 		antialiasing = (Preferences.globalAntialiasing && !PlayState.isPixelStage);
+	}
+
+	public override function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
 	}
 }

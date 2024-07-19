61bf7405d042d69ee6c949dc38797234101a893f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-19 22:12:28 +0800

    Update DebugInfoState.hx

--- source/funkin/menus/title/DebugInfoState.hx
+++ source/funkin/menus/title/DebugInfoState.hx
@@ -118,7 +118,7 @@ class DebugInfoState extends MusicBeatState
 								}
 							}
 						);
-						if (Preferences.flashing) {
+						if (GlobalSettings.FLASHING_LIGHTS) {
 							bg.color = 0xFFFFFFFF;
 							Tween.color(bg, 2.75, 0xFFFFFFFF, 0xFF000000, {ease: EaseUtil.getFlxEaseByString('cubeOut')});
 							flixel.FlxG.camera.flash(0xFFFFFFFF, 2.75, null, true);

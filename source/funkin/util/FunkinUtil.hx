2f68c4004b0cc7a3bb9504862014f92492a90712
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-26 20:17:08 +0800

    [Patch]: Code cleanup for FunkinUtil.hx

--- source/funkin/util/FunkinUtil.hx
+++ source/funkin/util/FunkinUtil.hx
@@ -2,17 +2,6 @@ package funkin.util;
 
 import openfl.media.Sound;
 import flixel.FlxG;
-import openfl.utils.Assets as OpenFLAssets;
-import lime.utils.Assets as LimeAssets;
-import lime.utils.AssetLibrary;
-import lime.utils.AssetManifest;
-import flixel.system.FlxSound;
-#if sys
-import sys.io.File;
-import sys.FileSystem;
-#else
-import openfl.utils.Assets;
-#end
 
 using StringTools;
 
@@ -71,14 +60,12 @@ class FunkinUtil
 	public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = [];
-		#if sys
-		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
-		#else
-		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
-		#end
 
-		for (i in 0...daList.length)
-		{
+		if (FileUtil.fileExists(path)) {
+			daList = FileUtil.getContentOfFile(path).trim().split('\n');
+		}
+
+		for (i in 0...daList.length) {
 			daList[i] = daList[i].trim();
 		}
 
@@ -88,30 +75,31 @@ class FunkinUtil
 	public static function listFromString(string:String):Array<String>
 	{
 		var daList:Array<String> = [];
+
 		daList = string.trim().split('\n');
 
-		for (i in 0...daList.length)
-		{
+		for (i in 0...daList.length) {
 			daList[i] = daList[i].trim();
 		}
 
 		return daList;
 	}
 
-	public static function dominantColor(sprite:flixel.FlxSprite):Int{
+	public static function dominantColor(sprite:flixel.FlxSprite):Int
+	{
 		var countByColor:Map<Int, Int> = [];
-		for(col in 0...sprite.frameWidth){
-			for(row in 0...sprite.frameHeight){
-			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-			  if(colorOfThisPixel != 0){
-				  if(countByColor.exists(colorOfThisPixel)){
-				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
-				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
-					 countByColor[colorOfThisPixel] = 1;
-				  }
-			  }
+		for (col in 0...sprite.frameWidth){
+			for (row in 0...sprite.frameHeight){
+			 	var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+				if (colorOfThisPixel != 0) {
+					if (countByColor.exists(colorOfThisPixel)){
+						countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
+					} else if (countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
+						countByColor[colorOfThisPixel] = 1;
+					}
+				}
 			}
-		 }
+		}
 		var maxCount = 0;
 		var maxKey:Int = 0;//after the loop this will store the max color
 		countByColor[flixel.util.FlxColor.BLACK] = 0;
@@ -134,24 +122,28 @@ class FunkinUtil
 		return dumbArray;
 	}
 
-	public static function soundExists(file:Null<Dynamic>):Bool {
+	public static function soundExists(file:Null<Dynamic>):Bool
+	{
 		if (file != null) {
-			return (Std.isOfType(file, Sound) || OpenFLAssets.exists(file));
+			return (Std.isOfType(file, Sound) || FileUtil.fileExists(file));
 		}
 		return false;
 	}
 
 	// uhhhh does this even work at all? i'm starting to doubt
 	// its not a void function you dumb idiot
-	public static function precacheSound(sound:String, ?library:String = null):Void {
+	public static function precacheSound(sound:String, ?library:String = null):Void
+	{
 		Paths.sound(sound, library);
 	}
 
-	public static function precacheMusic(sound:String, ?library:String = null):Void {
+	public static function precacheMusic(sound:String, ?library:String = null):Void
+	{
 		Paths.music(sound, library);
 	}
 
-	public static function browserLoad(site:String) {
+	public static function browserLoad(site:String):Void
+	{
 		#if linux
 		Sys.command('/usr/bin/xdg-open', [site]);
 		#else

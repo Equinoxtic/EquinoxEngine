1af86a09ff418924ce3e81f79c90455ccf40c360
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-27 17:18:22 +0800

    [Feat]: Add 'modFileExists()' method for FileUtil.hx

--- source/funkin/util/FileUtil.hx
+++ source/funkin/util/FileUtil.hx
@@ -21,9 +21,18 @@ class FileUtil
 	public static function fileExists(path:String):Bool
 	{
 		#if (sys)
-		return ( FileSystem.exists(path) );
+		return (FileSystem.exists(path));
 		#else
-		return ( OpenFLAssets.exists(path) );
+		return (OpenFLAssets.exists(path));
+		#end
+	}
+
+	public static function modFileExists(path:String):Bool
+	{
+		#if (MODS_ALLOWED)
+		return (FileSystem.exists(Paths.modsJson(path)) || FileSystem.exists(Paths.json(path)));
+		#else
+		return (OpenFlAssets.exists(Paths.json(path)));
 		#end
 	}
 }

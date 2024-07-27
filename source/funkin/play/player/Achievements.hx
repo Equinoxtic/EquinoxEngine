9398d9a0ee3cad911066909781dc119e2275aba0
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-28 00:03:03 +0800

    [Style]: Update indentation in Achievements.hx

--- source/funkin/play/player/Achievements.hx
+++ source/funkin/play/player/Achievements.hx
@@ -10,7 +10,8 @@ import flixel.text.FlxText;
 
 using StringTools;
 
-class Achievements {
+class Achievements
+{
 	public static var achievementsStuff:Array<Dynamic> = [ //Name, Description, Achievement save tag, Hidden achievement
 		["Freaky on a Friday Night",	"Play on a Friday... Night.",						'friday_night_play',	 true],
 		["She Calls Me Daddy Too",		"Beat Week 1 on Hard with no Misses.",				'week1_nomiss',			false],
@@ -29,23 +30,28 @@ class Achievements {
 		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				false],
 		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
 	];
+
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
 	public static var henchmenDeath:Int = 0;
-	public static function unlockAchievement(name:String):Void {
+
+	public static function unlockAchievement(name:String):Void
+	{
 		FlxG.log.add('Completed achievement "' + name +'"');
 		achievementsMap.set(name, true);
 		FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
 	}
 
-	public static function isAchievementUnlocked(name:String) {
-		if(achievementsMap.exists(name) && achievementsMap.get(name)) {
+	public static function isAchievementUnlocked(name:String):Bool
+	{
+		if (achievementsMap.exists(name) && achievementsMap.get(name)) {
 			return true;
 		}
 		return false;
 	}
 
-	public static function getAchievementIndex(name:String) {
+	public static function getAchievementIndex(name:String):Int
+	{
 		for (i in 0...achievementsStuff.length) {
 			if(achievementsStuff[i][2] == name) {
 				return i;
@@ -54,7 +60,8 @@ class Achievements {
 		return -1;
 	}
 
-	public static function loadAchievements():Void {
+	public static function loadAchievements():Void
+	{
 		if(FlxG.save.data != null) {
 			if (FlxG.save.data.achievementsMap != null) {
 				achievementsMap = FlxG.save.data.achievementsMap;
@@ -65,7 +72,8 @@ class Achievements {
 		}
 	}
 
-	public static function saveAchievements():Void {
+	public static function saveAchievements():Void
+	{
 		FlxG.save.data.achievementsMap = Achievements.achievementsMap;
 		FlxG.save.data.henchmenDeath = Achievements.henchmenDeath;
 	}

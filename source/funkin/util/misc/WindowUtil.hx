24d64433f561ce8b20a70d767ba56414c8cfb6c3
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-08-14 17:03:34 +0800

    [Feat]: Update code for the WindowUtil class

--- source/funkin/util/misc/WindowUtil.hx
+++ source/funkin/util/misc/WindowUtil.hx
@@ -11,22 +11,44 @@ class WindowUtil
 	public static function moveWindow(x:Float, y:Float):Void
 	{
 		#if (desktop)
-		Application.current.window.x = Std.int(x); Application.current.window.y = Std.int(y);
-		#else _traceUnsupportedMessage() #end
+		// Set limits for x and y position.
+		if (Math.isNaN(x))
+			x = 0.0;
+		if (Math.isNaN(y))
+			y = 0.0;
+		Application.current.window.x = Std.int(x);
+		Application.current.window.y = Std.int(y);
+		#else
+		_traceUnsupportedMessage();
+		#end
 	}
 
 	public static function setWindowSize(h:Float, w:Float):Void
 	{
 		#if (desktop)
-		Application.current.window.height = Std.int(h); Application.current.window.width = Std.int(w);
-		#else _traceUnsupportedMessage() #end
+		// Set limits for height and width.
+		if (h > 5120 || Math.isNaN(h)) {
+			h = 1920;
+		}
+
+		if (w > 1990 || Math.isNaN(w)) {
+			w = 1080;
+		}
+
+		Application.current.window.height = Std.int(h);
+		Application.current.window.width = Std.int(w);
+		#else
+		_traceUnsupportedMessage();
+		#end
 	}
 
 	public static function toggleFullscreen():Void
 	{
 		#if (desktop)
 		Application.current.window.fullscreen = !_fullscreenToggled;
-		#else _traceUnsupportedMessage() #end
+		#else
+		_traceUnsupportedMessage();
+		#end
 	}
 
 	private static function _traceUnsupportedMessage():Void

8774e5faa841c3c2ff5d2daf19e01f602421f2cf
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-08-14 20:57:36 +0800

    [Chore]: Update and add documentation for WindowUtil class

--- source/funkin/util/misc/WindowUtil.hx
+++ source/funkin/util/misc/WindowUtil.hx
@@ -9,11 +9,20 @@ class WindowUtil
 	#if (desktop)
 	private static var _fullscreenToggled:Bool = false;
 
-	public static function moveWindow(x:Float, y:Float):Void
+	/**
+	 * Sets the window's position based on the x and y position in space relative to the screen.
+	 * @param x The x position in space.
+	 * @param y The y position in space.
+	 */
+	public static function setWindowPosition(x:Float, y:Float):Void
 	{
 		setWindowX(Std.int(x)); setWindowY(Std.int(y));
 	}
 
+	/**
+	 * Sets the window's position in the x position in space relative to the screen.
+	 * @param x The x position in space.
+	 */
 	public static function setWindowX(x:Int):Void
 	{
 		if (Math.isNaN(x)) {
@@ -23,6 +32,10 @@ class WindowUtil
 		Application.current.window.x = x;
 	}
 
+	/**
+	 * Sets the window's position in the y position in space relative to the screen.
+	 * @param y The y position y space.
+	 */
 	public static function setWindowY(y:Int):Void
 	{
 		if (Math.isNaN(y)) {
@@ -32,11 +45,20 @@ class WindowUtil
 		Application.current.window.y = y;
 	}
 
+	/**
+	 * Sets the window's height and width.
+	 * @param h The height of the window to set.
+	 * @param w The width of the window to set.
+	 */
 	public static function setWindowSize(h:Float, w:Float):Void
 	{
 		setWindowHeight(Std.int(h)); setWindowWidth(Std.int(w));
 	}
 
+	/**
+	 * Sets the window's height.
+	 * @param h The value of the height.
+	 */
 	public static function setWindowHeight(h:Int):Void
 	{
 		if (h >= Variables.MAX_WINDOW_HEIGHT || Math.isNaN(h)) {
@@ -46,6 +68,10 @@ class WindowUtil
 		Application.current.window.height = h;
 	}
 
+	/**
+	 * Sets the window's width.
+	 * @param w The value of the width.
+	 */
 	public static function setWindowWidth(w:Int):Void
 	{
 		if (w >= Variables.MAX_WINDOW_WIDTH || Math.isNaN(w)) {
@@ -55,6 +81,9 @@ class WindowUtil
 		Application.current.window.width = w;
 	}
 
+	/**
+	 * Toggles whether or not the window should be fullscreen.
+	 */
 	public static function toggleFullscreen():Void
 	{
 		Application.current.window.fullscreen = !_fullscreenToggled;

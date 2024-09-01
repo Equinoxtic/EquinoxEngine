a5a3661fd55a5d8b4a82250e7fbd5785e6df3d25
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-09-01 19:00:25 +0800

    [Feat]: Create TextList.hx

---
+++ source/funkin/ui/display/misc/TextList.hx
@@ -0,0 +1,18 @@
+package funkin.ui.display.misc;
+
+import flixel.text.FlxText;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+
+class TextList extends FlxTypedSpriteGroup<FlxText>
+{
+	public function new(?x:Float, ?y:Float, ?listOfTexts:Null<Array<String>>):Void
+	{
+		super(x, y);
+
+		for (i in 0...listOfTexts.length) {
+			var text:FunkinText = new FunkinText(0, 0, 0, '${listOfTexts[i]}\n', 14, LEFT, true, 1.35);
+			text.y += i * 11;
+			add(text);
+		}
+	}
+}
